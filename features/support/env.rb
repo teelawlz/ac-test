79fc664b6da9f9ad72874e63ccdcbf09a153d403
Thomas Lawler <thomaslawler@outlook.com>
2017-09-03 19:51:38 -0700

    Update README, clean up env.rb some

--- features/support/env.rb
+++ features/support/env.rb
@@ -14,14 +14,4 @@ Capybara.register_driver :selenium do |app|
 	Capybara::Selenium::Driver.new(app, :browser => ENV['BROWSER'].to_sym)
 end
 
-# Phantomjs was attempted, but had issues getting it to locate elements
-# Capybara.javascript_driver = :poltergeist
-# Capybara.default_driver = :poltergeist
-# Capybara.register_driver :poltergeist do |app|
-#   options = {
-#       :js_errors => false,
-#       :phantomjs_options => ['--load-images=no', '--disk-cache=false']
-#   }
-#   Capybara::Poltergeist::Driver.new(app, options)
-# end
 Capybara.default_max_wait_time = 15
